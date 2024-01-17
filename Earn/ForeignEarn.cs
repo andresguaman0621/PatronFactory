26ee1473107034ef4315f43648ed13e2f83a32b3
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2024-01-17 08:25:36 -0500

    copia de carpeta Earn

---
+++ Earn/ForeignEarn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    public class ForeignEarn : IEarn
+    {
+        private decimal _percentage;
+        private decimal _extra;
+
+        public ForeignEarn(decimal percentage, decimal extra)
+        {
+            _percentage = percentage;
+            _extra = extra;
+        }
+
+        public decimal Earn(decimal amount)
+        {
+            decimal extra = _extra;
+            _extra = 0m;
+            return (amount * _percentage) + _extra;
+        }
+    }
+}
